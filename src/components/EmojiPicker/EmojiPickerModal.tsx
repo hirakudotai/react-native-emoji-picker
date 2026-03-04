4c9825a934c69c904f253601edfc54fcd73e7ce0
stefbuzas <skriver.web3@gmail.com>
2026-03-04 16:31:19 +0200

    Updated: Changelog

--- src/components/EmojiPicker/EmojiPickerModal.tsx
+++ src/components/EmojiPicker/EmojiPickerModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, Modal, TouchableOpacity, Text, StyleSheet, Animated, useWindowDimensions } from 'react-native';
 import { XIcon } from '../../assets/icons';
 import { EmojiPickerThemeProvider, useEmojiPickerTheme } from '../../theme';
